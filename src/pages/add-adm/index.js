82015951ff8c27d2a7accc424d48b07b7cdf63d2
DOLK2K <Marcosferreira.1234filho@gmail.com>
2023-10-20 09:50:34 -0300

    tela do produto jsx

--- src/pages/add-adm/index.js
+++ src/pages/add-adm/index.js
@@ -8,32 +8,74 @@ import Lixeira from '../../assets/images/lata-de-lixo 1.png'
 // tem coisas q não está igual no figma, e não está alinhado direito
 export default function Produtoo() {
     return (
-    <div className='aaaa'>
+    <div className='Cadastro-produtos'>
+        <div className='cadastro-produto-cabecalho'>
             <h1>CADASTRO DO PRODUTO</h1>
 
             <hr/>
-
-            <div>
+            </div>
+            <div id='chuvisco'>
                 <input type='file' />
 
-                <div>
+                <div className='patre-do-meio-produto'>
+
+                    <div className='relaxa'>
+                        <div className='descricao-produto'>
                     <p>Descrição do Produto</p>
                     <input type='text' />
-
+                    </div>
+                    <label>
+                        UNIDADE
+                    </label>
+                    <input type='number' /> 
+                    </div>
                     <div>
                         <div>
-                            <p>Tipo do produto</p>
-                            <input type='text' />
+
+                            <span>
+                            <label>Tipo do produto</label>
+                            
+                            <select>
+                                <option>Camiseta</option>
+                                <option>Calça</option>
+                                <option>Sapato</option>
+                                <option>Chapéu</option>
+                                <option>Óculos</option>
+                                <option>Barraca</option>
+                                <option>Lanterna</option>
+                                <option>Mochila</option>
+                                <option>Jaqueta</option>
+                            </select>
+                            </span>
                         </div>
 
                         <div>
-                            <p>categoria</p>
-                            <input type='text' />
+
+                            <span>
+                            <label>categoria</label>
+                            <select>
+                                <option > Vestuario</option>
+                                <option>Acessorio</option>
+                                <option>Profissional</option>
+                                
+                            </select>
+                            </span>
+                        </div>
+
+                        <div>
+                            <nav>
+                            <label>
+                                Situação
+                            </label>
+                            <section>Ativo</section>
+                            <section>Desativo</section>
+                            </nav>
                         </div>
                     </div>
-                    <hr />
+                    
 
                 </div>
+                <hr id='hrr' />
             </div>
 
             <div>
@@ -48,8 +90,17 @@ export default function Produtoo() {
                 </div>
 
                 <div>
-                    
+                    <p>Valor total</p>
+                    <input type='number'/>
                 </div>
+            </div>
+
+            <div>
+                <button>
+                    Cancelar
+                </button>
+
+                <button>Salvar</button>
             </div>
     </div>
     )
