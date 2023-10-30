8e6a85fc4e8e7a21764b26964efee6e28c812eed
songuasta17 <gulopes3006@gmail.com>
2023-10-30 08:47:03 -0300

    tsp

--- src/pages/meuspedidos/index.js
+++ src/pages/meuspedidos/index.js
@@ -12,6 +12,13 @@ import Mochila from '../../assets/images/moochila.png'
 import Cabo from '../../assets/images/cabo.png'
 import Camiseta from '../../assets/images/camiseta.png'
 import Botas from '../../assets/images/botas.png'
+import vanguard from '../../assets/images/vanguard.png'
+import correio from '../../assets/images/correio.png'
+import face from '../../assets/images/facebook.png'
+import twitter from '../../assets/images/twitter.png'
+import insta from '../../assets/images/insta.png'
+import youtube from '../../assets/images/youtube.png'
+
 export default function(){
     return(
         <div className='meupedido'>
@@ -61,31 +68,55 @@ export default function(){
 
 <div className='a97y'>
 <h1> <img src={Feito} />
-LIBERADO </h1> <h3>  <img src={Mochila} /> MOCHILA TÁTICA ASSAULT </h3>
+Liberado Pedido:31620000085</h1> <h3>  <img src={Mochila} /> Mochila Tática Assault </h3>
 <h4>SAÍDA:07-12-2023  ENTREGA:13-12-2023</h4>
 </div>
-
+      
 <div className='a440y'>
 <h1><img src={Pendente} />
-PENDENTE  </h1> <h3> <img src={Cabo} /> CABO TATICO MILITAR </h3>
-<h4>SAÍDA:NÃO DEFINIDA  ENTREGA:NÃO DEFINIDA</h4>
+Pendente Pedido:23910000083 </h1> <h3> <img src={Cabo} /> Cabo Tatico Militar </h3>
+<h4>SAÍDA:Não Definida  ENTREGA:Não Definida</h4>
 </div>
 
 <div className='a364y'>
 <h1><img src={Caminhao} />
-A CAMINHO </h1> <h3> <img src={Camiseta} /> CAMISETA SOLDIER  </h3>
+A caminho Pedido:23810000049</h1> <h3> <img src={Camiseta} /> Camiseta Soldier  </h3>
 <h4>SAÍDA:04-06-2023  ENTREGA:10-06-2023</h4>
 
 </div>
 <div className='a123y'>
 <h1> <img src={Concluida} />
-ENTREGA CONCLUÍDA  </h1> <h3> <img src={Botas} />BOTA TATICA MASCULINA </h3>
+ Concluído Pedido:21810000062  </h1> <h3> <img src={Botas} />Bota Tática Masculina </h3>
 <h4>SAÍDA:02-06-2023  ENTREGA:08-06-2023</h4>
 
 </div>
 
 </div>
 </div>
+
+<div className='emmbaix'>
+
+            <img src={vanguard}/>          
+
+            <img src={correio}/>
+
+            <img src={face}/>
+
+            <img src={twitter}/>
+
+            <img src={insta}/>
+
+            <img src={youtube}/>
+
+            
+          <div className='elements'>
+          <p>+012 345 6789</p>
+          <p>sac@vanguardtactical.com</p>
+          <p>Avenida Coronel Octaviano de Freitas Costa, 69, Socorro, São Paulo</p>
+            
+          </div>
+
+          </div>
 
 </div>
     )}
