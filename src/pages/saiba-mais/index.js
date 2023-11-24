e83dbe6238c2a1dbdefba36dfbcadffd08e89cb9
Vinícius <Marcosferreira.1234filho@gmail.com>
2023-11-24 08:42:21 -0300

    dd

--- src/pages/saiba-mais/index.js
+++ src/pages/saiba-mais/index.js
@@ -19,8 +19,39 @@ export default function Saiba() {
         <main>
            
 
-          NAO MEXE NESSA POOHA 
+       <h1>   Sobre Nossa Empresa de Produtos Táticos </h1>
 
+ <p>  Bem-vindo ao universo da excelência tática, onde cada produto é cuidadosamente projetado para atender às demandas dos profissionais mais exigentes. Nossa empresa de produtos táticos está comprometida em fornecer soluções de alta qualidade para atender às necessidades específicas de militares, agentes de segurança, entusiastas ao ar livre e todos que valorizam desempenho e confiabilidade.
+ </p>
+
+ <h1>  Missão </h1>
+
+  <p>  Nossa missão é proporcionar um diferencial crucial para aqueles que dependem de equipamentos táticos em situações críticas. Através de pesquisa avançada, inovação constante e padrões de qualidade rigorosos, buscamos elevar os padrões da indústria e garantir que nossos clientes estejam sempre um passo à frente.
+  </p> 
+
+ <h1>  Compromisso com a Qualidade </h1>
+
+ <p>  A qualidade é a espinha dorsal de nossa empresa. Cada produto é submetido a testes extensivos para garantir resistência, durabilidade e desempenho excepcionais. Nosso compromisso inabalável com a excelência reflete-se em cada detalhe, desde o design ergonômico até a seleção dos materiais mais avançados disponíveis.
+ </p>
+
+  <h1>  Inovação Constante </h1>
+
+
+<p>
+Estamos na vanguarda da inovação tática, sempre buscando maneiras de aprimorar nossos produtos e introduzir tecnologias de ponta. Acreditamos que a inovação é a chave para enfrentar os desafios em constante evolução enfrentados pelos profissionais que confiam em nossos produtos.
+</p>
+
+  <h1> Variedade de Produtos </h1>
+
+  <p>  Oferecemos uma ampla gama de produtos táticos, desde vestuário e calçados especializados até equipamentos de transporte e acessórios essenciais. Seja você um operador tático profissional ou um entusiasta ao ar livre dedicado, temos algo para atender às suas necessidades específicas.
+  </p>
+
+ <h1>   Comprometimento com o Cliente </h1>
+
+  <p>  Nosso compromisso com o cliente vai além da venda. Estamos dedicados a fornecer suporte excepcional, garantindo que cada cliente tenha uma experiência positiva ao escolher nossos produtos. Sua satisfação e confiança são nossa maior recompensa.
+
+Explore o mundo dos produtos táticos de alta qualidade e desempenho. Junte-se a nós enquanto capacitamos aqueles que buscam a excelência em cada missão, aventura ou desafio.
+</p>
 
         </main>
         
